16ce63dd9d5419c3673d7aa2602db405ca11d9d8
Jitendra Kumar Sharma <136931188+Jitendra-Sharma726@users.noreply.github.com>
2026-09-02 09:48:30 +0530

    Create SubmissionRepository.java

---
+++ SubmissionRepository.java
@@ -0,0 +1,8 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.Submission;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubmissionRepository extends JpaRepository<Submission, Long> {
+    boolean existsByTeamNameIgnoreCase(String teamName);
+}
